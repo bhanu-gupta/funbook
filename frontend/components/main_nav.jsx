65bc9e8651d0a243a9dd0a172d156ccb033de319
Bhanu Gupta <guptabhanu90@gmail.com>
2019-05-31 07:41:54 -0400

    add profile page header with styling

--- frontend/components/main_nav.jsx
+++ frontend/components/main_nav.jsx
@@ -19,40 +19,36 @@ class MainNav extends React.Component {
     }
 
     toggleMenu(event) {
-        console.log('in toggle');
-        // alert(this.state.showDropdown);
         let show = 0;
         if(this.state.showDropdown === 0) {
             show = 1;
         }
-        // alert(show);
         this.setState({showDropdown: show})
-        // alert(this.state.showDropdown)
     }
 
     hideMenu(event) {
-        console.log('in hide');
         event.stopPropagation();
         this.setState({ showDropdown: 0 })
     }
 
     render() {
         let dropdown = "";
-        console.log(`render ${this.state.showDropdown}`);
         if(this.state.showDropdown === 1) {
             dropdown = (
             <ul className="dropdown-nav">
                 <li><Link to="#">Your Groups</Link></li>
-                <li className="separator"></li>
+                <span className="separator"></span>
                 <li><Link to="#">Advertising on Facebook</Link></li>
-                <li className="separator"></li>
+                <span className="separator"></span>
                 <li><Link to="#">Activity Log</Link></li>
                 <li><Link to="#">News Feed Preferences</Link></li>
                 <li><Link to="#">Settings</Link></li>
-                <li onClick={this.props.logout}>Logout</li>
+                <li onClick={this.props.logout}><Link to="#">Logout</Link></li>
             </ul>
             );
         }
+
+        const dropdownIconClass = this.state.showDropdown ? 'col-white' : '';
         return (
         <header className="main-header">
             <nav className="main-nav">
@@ -64,17 +60,29 @@ class MainNav extends React.Component {
                     </h1>
                     <form className="search-bar">
                         <input type="text" placeholder="Search"/>
-                        <input type="submit" value=""/>
+                            <i className="fas fa-search search-icon"></i>
                     </form>
                 </div>
                 <ul className="nav-links">
-                    <li>
+                    <li className="pdg-top">
                         <Link to={`/${this.props.currentUser.username}`}>{this.props.currentUser.firstName}</Link>
                     </li>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="#">Create</Link></li>
-                        <li className="dropdown-icon" onClick={this.toggleMenu}>
-                            <Link to="#"><i></i></Link>
+                    <li className="nav-border pdg-top"><Link to="/">Home</Link></li>
+                    <li className="nav-border pdg-top"><Link to="#">Create</Link></li>
+                    <li className="nav-border icons">
+                            <i className="fas fa-user-friends fa-lg nav-icons"></i>
+                    </li>
+                    <li className="icons">
+                        <i className="fab fa-facebook-messenger fa-lg nav-icons"></i>
+                    </li>
+                    <li className="icons">
+                        <i className="fas fa-bell fa-lg nav-icons"></i>
+                    </li>
+                    <li className="nav-border icons">
+                            <i className="fas fa-question-circle fa-lg nav-icons"></i>
+                    </li>
+                        <li className="dropdown-icon icons" onClick={this.toggleMenu}>
+                            <i className={`fas fa-caret-down nav-icons ${dropdownIconClass}`}></i>
                         {dropdown}
                     </li>
                 </ul>
